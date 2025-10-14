7399e94d9e4348c9ccf69bfc4fe36cacb74958d9
zxslh <zxs_lh@msn.com>
2025-10-14 20:35:22 +0800

    Update split.py

--- split.py
+++ split.py
@@ -2,8 +2,20 @@ import re
 import os
 f_771 = ''
 f_crv = ''
+ip_info = {}
+ip_group = []
 with open('goodips', 'r', encoding='utf-8') as read_file:
     for line in read_file:
+        pattern = r'^(?P<ip>\d+\.\d+\.\d+\.\d+)(:(?P<port>\d+))?(#(?P<name>.+))?$'
+        match = re.match(pattern, s)
+        if match:
+            ip = match.group('ip')       # 提取IP地址
+            port = match.group('port')   # 提取端口（不存在时为None）
+            name = match.group('name')   # 提取名称（不存在时为None）
+            ip_info = {'IP': ip, '端口': port, '名称': 'name'}
+            ip_group.append(ip_info)
+            
+        
         if '771' in line:
             f_771 += f'{line}\n'  # 新行追加到末尾，保持原顺序
         if 'crv' in line:
